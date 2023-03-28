52fa495c6097d30f7f0ae0c31c6fcfc0bfe56825
Neomar706 <neomarp706@gmail.com>
2023-03-28 00:28:39 -0400

    Last Deploy

--- src/app.js
+++ src/app.js
@@ -27,18 +27,12 @@ app.set('port', process.env.PORT || 5000)
 
 
 /** Middlewares */
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ limit: '60mb' }))
 app.use(cookieParser(process.env.SECRET_SESSION_KEY))
 process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
 app.use(cors({
-    // origin: process.env.FRONTEND_HOST,
-    origin: '*',
+    origin: process.env.FRONTEND_HOST,
     credentials: true
 }))
 app.use(sessionMiddleware())
