65f85b700a1d92732578fdab3a635e2f6ffb1220
Neomar706 <neomarp706@gmail.com>
2023-03-28 00:20:47 -0400

    Added Middleware To Allow All Origin

--- src/app.js
+++ src/app.js
@@ -27,6 +27,11 @@ app.set('port', process.env.PORT || 5000)
 
 
 /** Middlewares */
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ limit: '60mb' }))
 app.use(cookieParser(process.env.SECRET_SESSION_KEY))
